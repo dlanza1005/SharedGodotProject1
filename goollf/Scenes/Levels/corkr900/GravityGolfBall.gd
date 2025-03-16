f621d30cf99593cb837d6aa747e027124de92520
corkr900 <109830893+corkr900@users.noreply.github.com>
2025-03-16 12:04:41 -0500

    fix accidental revert of gravity golf ball script

--- goollf/Scenes/Levels/corkr900/GravityGolfBall.gd
+++ goollf/Scenes/Levels/corkr900/GravityGolfBall.gd
@@ -1,15 +1,19 @@
 extends RigidBody2D
 class_name GravityGolfBall
 
-var gravityMultiplier = 20000
-var resistance = 0.4
-var aiming = true
+var gravityMultiplier: float = 30000
+var aimMultiplier: float = 120
+var resistance: float = 0.4
+var aiming: bool = false
+var maxAimDistance: float = 150
+var stopBouncingThreshold: float = 5
+var angularVelocityMulOnBounce: float = 0.6
 
-func _ready():
-	# TODO
-	pass
+var level_is_active: bool = false
 
-func _process(_delta):
+func _process(_delta) -> void:
+	if not level_is_active:
+		return
 	if aiming:
 		constant_force = Vector2(0, 0)
 		handleAim()
@@ -17,9 +21,8 @@ func _process(_delta):
 		var force = calcForce() * gravityMultiplier
 		force += drag()
 		constant_force = force
-	pass
 
-func calcForce():
+func calcForce() -> Vector2:
 	if (aiming):
 		return Vector2(0, 0)
 	var sum = Vector2(0, 0)
@@ -31,25 +34,49 @@ func calcForce():
 		sum += force
 	return sum
 
-func drag():
+func drag() -> Vector2:
 	return -linear_velocity * resistance
 
-func _input(event):
+func _input(event) -> void:
+	if not level_is_active or not aiming:
+		return
 	if event is InputEventMouseButton:
-		aiming = false
-		get_parent().get_node("Aimer").set_visible(false)
-		var offset = event.global_position - self.global_position
-		var impulse = pow(offset.length(), 0.5) * 25
-		apply_impulse(offset.normalized() * impulse)
+		var clickPos = get_local_mouse_position() + position
+		var offset = (clickPos - self.global_position).limit_length(maxAimDistance)
+		var speed = pow(offset.length(), 0.5) * aimMultiplier
+		shoot(offset.normalized() * speed)
 
-func _on_body_entered(_body: Node) -> void:
+func _on_body_entered(body: Node) -> void:
+	if not level_is_active:
+		return
+	var norm: Vector2 = (position - body.position).normalized()
+	var projection = linear_velocity.dot(norm) * norm
+	if projection.length_squared() < stopBouncingThreshold * stopBouncingThreshold:
+		beginAim()
+	else:
+		angular_velocity *= angularVelocityMulOnBounce
+
+func beginAim() -> void:
 	aiming = true
-	get_parent().get_node("Aimer").set_visible(true)
+	set_deferred("freeze", true)
 	linear_velocity = Vector2(0,0)
 	angular_velocity = 0
+	rotation_degrees = 0
+	get_parent().get_node("Aimer").set_visible(true)
 
-func handleAim():
+func shoot(vel: Vector2) -> void:
+	aiming = false
+	set_deferred("freeze", false)
+	get_parent().get_node("Aimer").set_visible(false)
+	set_deferred("linear_velocity", vel)
+	set_deferred("angular_velocity", randf_range(-20, 20))
+
+func handleAim() -> void:
 	var aimer = get_parent().get_node("Aimer")
-	var mouseposition = get_viewport().get_mouse_position()
-	var offset = (mouseposition - position).limit_length(150)
+	var mouseposition = get_local_mouse_position() + position
+	var offset = (mouseposition - position).limit_length(maxAimDistance)
 	aimer.position = offset + position
+
+func _on_gravity_golf_level_active() -> void:
+	level_is_active = true
+	beginAim()
