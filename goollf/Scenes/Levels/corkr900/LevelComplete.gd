309fef2ef9532466d9239292ab4dab5293665594
corkr900 <109830893+corkr900@users.noreply.github.com>
2025-03-16 12:05:13 -0500

    Update gravity golf to work with main menu

--- goollf/Scenes/Levels/corkr900/LevelComplete.gd
+++ goollf/Scenes/Levels/corkr900/LevelComplete.gd
@@ -1,6 +1,6 @@
 extends PanelContainer
 
+@export var continue_button: Button
 
-func _on_button_pressed() -> void:
-	# TODO: go to the next level or whatever
-	pass
+func _on_okay_pressed() -> void:
+	GameState.load_next_or_return()
